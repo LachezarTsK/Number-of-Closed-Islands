be2c0f7549470649b3c331a8d856f9316a602770
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-03-08 11:40:34 +0100

    Update Solution.js

--- Solution.js
+++ Solution.js
@@ -6,7 +6,6 @@
 var closedIsland = function (grid) {
     this.LAND = 0;
     this.WATER = 1;
-    this.CLOSED_ISLAND = 2;
 
     this.rows = grid.length;
     this.columns = grid[0].length;
@@ -14,7 +13,7 @@ var closedIsland = function (grid) {
 
     for (let r = 1; r < this.rows - 1; r++) {
         for (let c = 1; c < this.columns - 1; c++) {
-            if (grid[r][c] === 0 && isAreaOfClosedIsland(grid, r, c)) {
+            if (grid[r][c] === this.LAND && isAreaOfClosedIsland(grid, r, c)) {
                 countClosedIslands++;
             }
         }
@@ -32,12 +31,11 @@ function isAreaOfClosedIsland(grid, r, c) {
     if (r < 0 || r === this.rows || c < 0 || c === this.columns) {
         return false;
     }
-    if (grid[r][c] === this.WATER || grid[r][c] === this.CLOSED_ISLAND) {
+    if (grid[r][c] === this.WATER) {
         return true;
     }
 
-    // 'grid[r][c] = WATER' will also work. 'CLOSED_ISLAND' is for the sake of completeness.
-    grid[r][c] = CLOSED_ISLAND;
+    grid[r][c] = this.LAND;
     let up = isAreaOfClosedIsland(grid, r - 1, c);
     let down = isAreaOfClosedIsland(grid, r + 1, c);
     let left = isAreaOfClosedIsland(grid, r, c - 1);
